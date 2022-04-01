2b4b17137afe9b30f20d271a6fad785d83210bc1
Joe Christianson <jcfargond@gmail.com>
2022-03-31 22:29:59 -0500

    made function to calculate severity and converted temps

--- assets/scripts/api-load.js
+++ assets/scripts/api-load.js
@@ -1,10 +1,25 @@
-
-
+console.log(moment())
+console.log(1648749600)
+var unixFormat = moment(0).format("MM/DD/YYYY HH:MM");
+console.log(unixFormat);
 class Day{
     constructor(index,dayData){
         this.index = index;
-        this.date = new Date(dayData.dt);
+        this.date = moment(dayData.dt*1000).format("MMM Do");
         this.icon = dayData.weather[0].icon;
+        this.low = toFarenheit(dayData.temp.min);
+        this.high = toFarenheit(dayData.temp.max);
+        this.windSpeed = dayData.wind_speed;
+        this.humidity = dayData.humidity;
+        this.severity = this.findSeverity(this.low,this.high,this.windSpeed)
+    }
+    findSeverity(low,high,wind,uvi){
+        if (low<15||high>95||wind>20)
+        return "severe";
+        else if(low<55||high>88||wind>10){
+            return "moderate"
+        }
+        else return "favorable";
     }
 }
 
@@ -16,22 +31,27 @@ class WeatherObj{
         this.currentConditions = data.current.weather[0].main;
         this.currentConditionsDesc = data.current.weather[0].description;
         this.currentConditionsIcon = data.current.weather[0].icon;
-        this.temp = data.current.temp;
+        this.temp = toFarenheit(data.current.temp);
         this.humidity = data.current.humidity;
-        this.windSpeed = data.current.windSpeed;
+        this.windSpeed = data.current.wind_speed;
         this.uvi = data.current.uvi;
         this.future= this.createDays(data)
         this.severity = this.findSeverity(this.temp,this.windSpeed,this.uvi)
     }
     createDays(data){
         const days = [];
-        for(let i = 0;i<5;i++){
+        for(let i = 1;i<6;i++){
             days.push(new Day(i,data.daily[i]))
         }
         return days;
     }
-    findSeverity(temp,wind,uvi){
+    findSeverity(temp,wind,uvi,humidity){
+        if (temp<15||temp>95||wind>20||uvi>8)
         return "severe";
+        else if(temp<55||temp>88||wind>10||uvi>6){
+            return "moderate"
+        }
+        else return "favorable";
     }
 }
 
@@ -63,3 +83,8 @@ const loadWeather = async (location)=>{
 }
 
 loadWeather("Minneapolis, MN")
+
+ function toFarenheit(kel){
+     console.log(kel)
+    return (kel-273.15)*9/5+32
+ }
