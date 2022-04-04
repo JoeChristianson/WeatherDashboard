410389e6009ee87ad3f420615e835d94ff43a48a
Joe Christianson <jcfargond@gmail.com>
2022-04-03 20:42:17 -0500

    Got previous search buttons working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+const prevSearches = JSON.parse(localStorage.getItem("prevWeatherSearches")) || ["Minneapolis, MN"];
+
 const searchBtn = $("#search-btn");
 const searchInput = $("#search-input")
 
@@ -9,12 +11,17 @@ const currentWind = $("#current-wind");
 const currentHumidity = $("#current-humidity");
 const currentUV = $("#current-uv")
 
-const icons = {
 
+const icons = {
+    
 }
 
-const newSearch = async ()=>{
-    const weather = await loadWeather(searchInput.val()?searchInput.val():"Minneapolis, MN");
+const newSearch = async (terms)=>{
+    let weather="x";
+    let search = terms?terms:(searchInput.val()?searchInput.val():prevSearches[0]);
+    console.log(search)
+    weather = await loadWeather(search);
+    console.log(weather);
     currentCity.text(`${weather.city}, ${weather.state}`);
     console.log(currentDate.text());
     currentDate.text(moment().format("MMM Do"));
@@ -32,21 +39,24 @@ const newSearch = async ()=>{
 
 console.log(searchInput);
 searchBtn.on("click", async e=>{
-    // const weather = await loadWeather(searchInput.val());
-    // currentCity.text(`${weather.city}, ${weather.state}`);
-    // console.log(currentDate.text());
-    // currentDate.text(moment().format("MMM Do"));
-    // currentTemp.text(Math.round(weather.temp)+"&#176; F");
-    // currentWind.text(weather.windSpeed+ " mph");
-    // currentHumidity.text(weather.humidity);
-    // currentUV.text(weather.uvi);
-    // for(let i = 0;i<5;i++){
-    //     $(`#date-${i}`).text(weather.future[i].date);
-    //     $(`#temp-${i}`).text(`${weather.future[i].low} / ${weather.future[i].high}`)
-    //     $(`#wind-${i}`).text(`${weather.future[i].windSpeed}`)
-    //     $(`#humidity-${i}`).text(`${weather.future[i].humidity}%`)
-    // }
-    await newSearch()
+    await newSearch(false)
+    prevSearches.unshift(searchInput.val());
+    localStorage.setItem("prevWeatherSearches",JSON.stringify(prevSearches));
+    searchInput.val("")
 })
 
 newSearch();
+loadButtons();
+
+function loadButtons(){
+    $("#past-searches").html("");
+    for(let i=0;i<(Math.min(10,prevSearches.length));i++){
+        const btn = $("<button>");
+        btn.text(prevSearches[i]);
+        btn.on("click",async e=>{
+            console.log(prevSearches)
+            await newSearch(prevSearches[i])
+        })
+        $("#past-searches").append(btn);
+    }
+}
